da2baf36cfa3c10d96b4f68e198045b9deb56a7c
DarveshAher <darvesh.aher25012005@gmail.com>
2025-11-05 16:39:28 +0530

    Delete backend directory

--- backend/src/validators/money.validators.js
+++
@@ -1,9 +0,0 @@
-import { body } from 'express-validator';
-
-export const recordValidator = [
-  body('kind').isIn(['income','expense']),
-  body('description').isLength({ min: 1 }),
-  body('amount').isFloat({ min: 0 }),
-  body('category').isString().notEmpty(),
-  body('date').isISO8601(),
-];
